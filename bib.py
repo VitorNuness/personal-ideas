a85419bdf278c7961ff854ffecfe83aa0ecde0cc
Vitor Nunes da Silva <123211068+VitorNuness@users.noreply.github.com>
2023-05-28 19:18:25 -0300

    criando funcao menu

--- bib.py
+++ bib.py
@@ -6,3 +6,20 @@ from hashlib import sha256
 class Usuario:
     user: str
     senha: str
+
+def menu():
+    opcoes = [1,2,0]
+    while True:
+        try:
+            print("MENU\n1 - Entrar\n2 - Cadastrar\n0 - Sair")
+            selecao = int(input("Opção: "))
+            if selecao in opcoes:
+                if selecao == 0:
+                    print("Até mais.")
+                    return None
+                else:
+                    return selecao
+            else:
+                print("Opção inválida.")
+        except ValueError:
+            print("Opção inválida.")
