ff0102f3f2fad56ec98e15dcce453904e433ffa0
Vitor Nunes da Silva <123211068+VitorNuness@users.noreply.github.com>
2023-05-28 19:19:11 -0300

    criando funcao cadastrar

--- bib.py
+++ bib.py
@@ -23,3 +23,26 @@ def menu():
                 print("Opção inválida.")
         except ValueError:
             print("Opção inválida.")
+
+def cadastrar_usuario():
+    while True:
+        print("Digite 0 para voltar.")
+        usuario = input("USUÁRIO: ").lower()
+        if usuario == "0":
+            break
+        else:
+            senha = getpass("SENHA: ")
+            senha = str(sha256(senha.encode()).digest())
+            if senha == "0":
+                break
+            else:
+                arquivo_usuarios = open("usuarios.txt", "r")
+                for item in arquivo_usuarios:
+                    usuarios = item.split(";")
+                    obj_usuario = Usuario(usuarios[0], usuarios[1])
+                    if usuario == obj_usuario.user:
+                        return print("Usuário já existe.")
+                #lista_usuarios.append(Usuario(usuario, senha))
+                with open('usuarios.txt', '+a') as arquivo:
+                    arquivo.write(usuario+";"+senha+";"+"\n")                    
+        return print("Cadastro realizado com sucesso.")
